33465c24e1371048159690b02e6ae799bd495132
Daniel Demidko <demidko@farpost.com>
2022-02-15 16:35:24 +1000

    Kotlin version updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,23 +3,23 @@ repositories {
   maven("https://jitpack.io")
 }
 plugins {
-  kotlin("jvm") version "1.6.10"
+  kotlin("jvm") version "1.6.20-M1"
   id("com.palantir.graal") version "0.10.0"
 }
 dependencies {
   implementation("com.github.vidstige:jadb:v1.2.1")
-  implementation("com.github.ajalt.clikt:clikt:3.3.0")
+  implementation("com.github.ajalt.clikt:clikt:3.4.0")
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
   testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
-  testImplementation("io.mockk:mockk:1.12.1")
+  testImplementation("io.mockk:mockk:1.12.2")
 }
 tasks.compileKotlin {
   kotlinOptions.jvmTarget = "17"
-  kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.time.ExperimentalTime"
+  kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.time.ExperimentalTime"
 }
 tasks.compileTestKotlin {
   kotlinOptions.jvmTarget = "17"
-  kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.time.ExperimentalTime"
+  kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.time.ExperimentalTime"
 }
 tasks.test {
   useJUnitPlatform()
