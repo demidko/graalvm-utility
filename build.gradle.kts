bde89c7e9ebf699bd91f4a61451daf250676dff1
Daniel Demidko <demidko@farpost.com>
2022-12-01 01:45:05 +0500

    Kotlin updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
   maven("https://jitpack.io")
 }
 plugins {
-  kotlin("jvm") version "1.7.20"
+  kotlin("jvm") version "1.7.22"
   id("com.palantir.graal") version "0.12.0"
 }
 dependencies {
